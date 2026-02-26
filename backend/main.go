0d92a1dbe83c0718a498c7b0a1fd3fa7e3db2815
Pandu Tria Adyatama <panduadyatama029@gmail.com>
2026-02-26 16:01:46 +0700

    update

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"gin-gorm/config"
+	"gin-gorm/models"
 	"gin-gorm/routes"
+	"gin-gorm/utils"
 	"log"
 
 	"github.com/gin-contrib/cors"
@@ -23,6 +25,28 @@ func main() {
 
 	config.ConnectDB()
 
+	
+	config.DB.AutoMigrate(
+		&models.User{},
+		&models.IdentitasProyek{},
+		&models.IdentitasProyekPhoto{},
+		&models.IdentitasProyekDocument{},
+		&models.RabHeader{},
+		&models.RabDetail{},
+		&models.ScheduleHeader{},
+		&models.ScheduleItem{},
+		&models.ScheduleWeek{},
+		&models.RealisasiHeader{},
+		&models.RealisasiDetail{},
+		&models.Evaluasi{},
+		&models.Pengaduan{},
+		&models.PengaduanMedia{},
+		&models.PengaduanTimeline{},
+		&models.PengaduanReview{},
+	)
+
+	SeedSuperAdmin()
+
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
@@ -34,3 +58,28 @@ func main() {
 
 	r.Run("0.0.0.0:8083")
 }
+
+func SeedSuperAdmin() {
+	var user models.User
+
+	email := "superadmin"
+
+	err := config.DB.Where("email = ?", email).First(&user).Error
+	if err == nil {
+		return
+	}
+
+	password := "SuperAdminss7617&"
+	role := "superadmin"
+	isActive := "true"
+
+	superAdmin := models.User{
+		Email:    &email,
+		Password: utils.HashSHA512(password),
+		Role:     &role,
+		IsActive: &isActive,
+	}
+
+	config.DB.Create(&superAdmin)
+}
+
