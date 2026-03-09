e0dfeec0bf6922cdae4040546d91c45cd7575194
setio7-dev <nugrahasetio7@gnail.com>
2026-03-09 08:29:54 +0700

    update

--- backend/config/database.go
+++ backend/config/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 func ConnectDB() {
 	// dsn := "host=100.74.236.13 user=postgres password=rayzen7 dbname=sipamas port=5432 sslmode=disable"
 	// dsn := "host=103.170.89.225 user=postgres password=engineermuda dbname=sipamas port=5432"
-	dsn := "host=localhost user=postgres password= dbname=sipamas port=5432"
+	dsn := "host=localhost user=sipamas_user password=sipamas_db dbname=sipamas port=5432"
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
